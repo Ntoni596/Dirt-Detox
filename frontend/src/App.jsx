07b7b9ea37e1079f53d294254ffda06cfc4fc4ed
Deepak Joshi <ntoni596@gmail.com>
2023-09-26 21:57:33 +1300

    Anchor Scrolling Fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,5 @@
 import './App.css'
-import {BrowserRouter, Routes, Route } from "react-router-dom"
 import Navigation from './components/Navigation'
-import About from './components/About'
-import Services from './components/Services'
-import Contact from './components/Contact'
 import Home from './pages/Home'
 
 
@@ -13,15 +9,8 @@ function App() {
 
   return (
     <div className='app'>
-      <BrowserRouter>
         <Navigation />
         <Home />
-        <Routes>
-          <Route path='/about' element={<About/>}/>
-          <Route path='/services' element={<Services/>}/>
-          <Route path='/contact' element={<Contact/>}/>
-        </Routes>
-      </BrowserRouter>
     </div>
   )
 }
--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -1,72 +1,70 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import navLogo from "../assets/Dirt-Detox-Logo.svg"
-import heroImage from "../assets/Hero-Section.png"
+import navLogo from '../assets/Dirt-Detox-Logo.svg';
+import heroImage from '../assets/Hero-Section.png';
 
 function Navigation() {
-    const [isScrolled, setIsScrolled] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
 
-    useEffect(() => {
-        const smoothScroll = (event) => {
-            event.preventDefault();
-            const targetId = event.target.getAttribute('href').substring(1);
-            const targetElement = document.getElementById(targetId);
-            if (targetElement) {
-                window.scrollTo({
-                    top: targetElement.offsetTop,
-                    behavior: 'smooth'
-                });
-            }
-        };
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollThreshold = 50; // Adjust as needed
+      if (window.scrollY > scrollThreshold) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
 
-        const handleScroll = () => {
-            const scrollThreshold = 50; // Adjust as needed
-            if (window.scrollY > scrollThreshold) {
-                setIsScrolled(true);
-            } else {
-                setIsScrolled(false);
-                
-            }
-        };
-
-        const links = document.querySelectorAll('a[href^="#"]');
-        links.forEach((link) => {
-            link.addEventListener('click', smoothScroll);
+    const smoothScroll = (targetId) => {
+      const targetElement = document.getElementById(targetId);
+      if (targetElement) {
+        window.scrollTo({
+          top: targetElement.offsetTop,
+          behavior: 'smooth',
         });
+      }
+    };
 
-        window.addEventListener('scroll', handleScroll);
+    const links = document.querySelectorAll('.nav-links a');
+    links.forEach((link) => {
+      link.addEventListener('click', (event) => {
+        event.preventDefault();
+        const targetId = event.target.getAttribute('href').substring(1);
+        smoothScroll(targetId);
+      });
+    });
 
-        return () => {
-            links.forEach((link) => {
-                link.removeEventListener('click', smoothScroll);
-            });
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
+    window.addEventListener('scroll', handleScroll);
 
-    return (
-        <div >
-            <div className='hero'>
-                <div className='hero-image'>
-                    <img src={heroImage} alt="Hero Image" />
-                </div>
-                <div className='hero-text'>
+    return () => {
+      links.forEach((link) => {
+        link.removeEventListener('click', smoothScroll);
+      });
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
-                </div>
-            </div>
-
-            <nav id={isScrolled ? 'scrolled-navigation' : 'navigation'}>
-                <div className='nav-logo'>
-                    <img src={navLogo} alt='Dirt Detox Logo' />
-                </div>
-                <div className="nav-links">
-                    <Link to="#about">About</Link>
-                    <Link to="#services">Services</Link>
-                    <Link to="#contact">Contact</Link>
-                </div>
-            </nav>
+  return (
+    <div>
+      <div className="hero">
+        <div className="hero-image">
+          <img src={heroImage} alt="Hero Image" />
         </div>
-    );
+        <div className="hero-text"></div>
+      </div>
+
+      <nav id={isScrolled ? 'scrolled-navigation' : 'navigation'}>
+        <div className="nav-logo">
+          <img src={navLogo} alt="Dirt Detox Logo" />
+        </div>
+        <div className="nav-links">
+          <a href="#about">About</a>
+          <a href="#services">Services</a>
+          <a href="#contact">Contact</a>
+        </div>
+      </nav>
+    </div>
+  );
 }
 
 export default Navigation;
